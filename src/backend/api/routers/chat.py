e2e4dbf0d39b27a117b49937c43065436e1c4f14
songbinghui <soongbinghui@gmail.com>
2023-12-12 00:49:45 +0800

    feat: openai

--- src/backend/api/routers/chat.py
+++ src/backend/api/routers/chat.py
@@ -1,8 +1,12 @@
-import asyncio
-import time
+import json
+import os
+from typing import List
 
+from dotenv import load_dotenv
 from fastapi import APIRouter, Request
 from fastapi.responses import StreamingResponse
+from openai import AzureOpenAI
+from openai.types.chat.chat_completion_message_param import ChatCompletionMessageParam
 
 chat_router = APIRouter(prefix="/chat", tags=["chat"])
 
@@ -11,20 +15,42 @@ STREAM_DELAY = 1
 RETRY_TIMEOUT = 15000
 
 
-def get_server_time():
-    return time.ctime()
+load_dotenv()  # take environment variables from .env.
+
+
+client = AzureOpenAI(
+    api_version=os.environ["OPENAI_API_VERSION"],
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_key=os.environ["AZURE_OPENAI_API_KEY"],
+    azure_deployment="gpt-35-turbo",
+)
+
+
+def completion(
+    messages: str | List[ChatCompletionMessageParam],
+    temperature=0,
+):
+    if isinstance(messages, str):
+        messages = [{"role": "user", "content": messages}]
+    stream = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=True,
+        temperature=temperature,
+    )
+
+    for chunk in stream:
+        if len(chunk.choices) > 0 and chunk.choices[0].delta.content:
+            content = chunk.choices[0].delta.content
+            # sys.stdout.write(content)
+            yield f"event: add\nretry: {RETRY_TIMEOUT}\ndata: {json.dumps(content,ensure_ascii=False)}\nid: {1}\n\n"
+    yield "event: finish\n\n"
 
 
 @chat_router.get("/stream")
 async def event_stream(request: Request):
-    async def event_generator():
-        for i in range(1, 4):
-            yield f"event: add\nretry: {RETRY_TIMEOUT}\ndata: {i}\nid: {i}\n\n"
-            await asyncio.sleep(STREAM_DELAY)
-        yield "event: finish\ndata: finish\n\n"
-
     headers = {
         "Content-Type": "text/event-stream; charset=utf-8",
         "Cache-Control": "no-cache",
     }
-    return StreamingResponse(event_generator(), headers=headers)
+    return StreamingResponse(completion("你好"), headers=headers)
